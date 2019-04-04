04a67a2985aee25a75e93bc384406cb4854ed219
Sebastian Poblete <sebapobletec@gmail.com>
2019-04-03 21:22:12 -0300

    show dashboard only to admins

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-
+  before_action :is_admin?
   def index
     @users = User.all
+  end
+  private
+  def is_admin?
+    if current_user.admin? == false
+      redirect_to root_path
+    end
   end
 
 end
